b994f17a3c88bb154accf21bd6a7ce9fd572bc01
samwijayag <sam.wijayag@gmail.com>
2019-05-06 01:03:12 -0700

    added animations

--- JS/scrypt.js
+++ JS/scrypt.js
@@ -76,8 +76,25 @@ $(document).ready(function(){
     },{
     	offset:'100%'
     })
+
+
+
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
    
    	$(this).scrollTop(0);
-   
 
 })
+
